0e7769840895282fd53b1300cc4885ac733789e9
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-16 16:30:20 -0700

    added get logs endpoint

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -3,9 +3,13 @@ const LogEntry = require('../models/LogEntry');
 
 const router = Router();
 
-router.get('/', async (req, res) => {
-  const entries = await LogEntry.find();
-  res.json(entries);
+router.get('/', async (req, res, next) => {
+  try {
+    const entries = await LogEntry.find();
+    res.json(entries);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post('/', async (req, res, next) => {
