dec09cf1b4d5f8bb6c47d838880abb5f433cf878
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-16 14:25:27 -0700

    added middlewares

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,29 @@
 const express = require('express');
+// morgan: log request to console
+const morgan = require('morgan');
+// helmet: secure request header
+const helmet = require('helmet');
+const cors = require('cors');
+const middlewares = require('./middlewares');
 
 const app = express();
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
 
-const port = process.env.PORT || 6000;
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello, this is root route',
+  });
+});
+
+// middlewares
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+const port = process.env.PORT || 6001;
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
