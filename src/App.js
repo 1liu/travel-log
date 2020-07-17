4c4e6da065ff67f8804f399b0c1de8d116077c44
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-16 20:26:21 -0700

    added Marker on map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import * as React from 'react';
-import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import * as React from "react";
+import { useState, useEffect, Fragment } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
 
 const App = () => {
+  const [logEntries, setLogEntries] = useState([]);
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
+    width: "100vw",
+    height: "100vh",
+    latitude: 37,
+    longitude: -95,
+    zoom: 3,
   });
 
+  const listLogEntries = () => {
+    fetch('/api/logs')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setLogEntries(data);
+      })
+      .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    listLogEntries();
+  }, []);
+
+  console.log(logEntries)
   return (
+
     <ReactMapGL
       {...viewport}
+      mapStyle="mapbox://styles/liuy23/ckcpig7jf0hha1ioy1gcdg1kq"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-      onViewportChange={nextViewport => setViewport(nextViewport)}
-    />
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+    >
+      {logEntries.map(entry => (
+        <Marker latitude={entry.latitude} longitude={entry.longitude} offsetLeft={-20} offsetTop={-10}>
+          <div>{entry.title}</div>
+        </Marker>
+      ))}
+    </ReactMapGL>
+
+
   );
-}
+};
 
 export default App;
