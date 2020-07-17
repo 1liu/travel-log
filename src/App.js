03dda5bb19707cb05634faffe6e4bd5e3b48def2
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-16 17:46:51 -0700

    tested mapbox sample code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import './App.css';
+import * as React from 'react';
+import { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 
-function App() {
+const App = () => {
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  });
+
   return (
-    <div className="App">
-
-    </div>
+    <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      onViewportChange={nextViewport => setViewport(nextViewport)}
+    />
   );
 }
 
