c4a217b68e2bf7b859380e9eb0350b5a12f8d453
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-16 21:41:58 -0700

    added log form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ReactMapGL, { Marker, Popup } from "react-map-gl";
 
 const App = () => {
   const [logEntries, setLogEntries] = useState([]);
+  const [addEntryLocation, setAddEntryLocation] = useState(null);
   const [showPopup, setShowPopup] = useState({});
   const [viewport, setViewport] = useState({
     width: "100vw",
@@ -17,17 +18,26 @@ const App = () => {
     fetch('/api/logs')
       .then(res => res.json())
       .then(data => {
-        console.log(data);
         setLogEntries(data);
       })
       .catch(err => console.log(err))
+  }
+
+  const showAddMarkerPopup = (event) => {
+    const { lngLat } = event;
+    setAddEntryLocation({ latitude: lngLat[1], longitude: lngLat[0] })
+
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('submitted')
   }
 
   useEffect(() => {
     listLogEntries();
   }, []);
 
-  console.log(logEntries)
   return (
 
     <ReactMapGL
@@ -35,14 +45,13 @@ const App = () => {
       mapStyle="mapbox://styles/liuy23/ckcpig7jf0hha1ioy1gcdg1kq"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      onDblClick={showAddMarkerPopup}
     >
       {logEntries.map(entry => (
-        <Fragment>
+        <Fragment key={entry._id}>
           <Marker
             latitude={entry.latitude}
             longitude={entry.longitude}
-            offsetLeft={-12}
-            offsetTop={-24}
           >
             <div
               onClick={() => setShowPopup({
@@ -54,10 +63,7 @@ const App = () => {
                 width={12 * viewport.zoom}
                 height={12 * viewport.zoom}
                 stroke="currentColor"
-                stroke-width="2"
                 fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
               >
                 <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
                 <circle cx="12" cy="10" r="3"></circle>
@@ -78,9 +84,57 @@ const App = () => {
               <small>Visited on: {new Date(entry.visitedDate).toLocaleDateString()}</small>
             </div>
           </Popup>}
+          {addEntryLocation && (
+            <Fragment>
+              <Marker
+                latitude={addEntryLocation.latitude}
+                longitude={addEntryLocation.longitude}
+              >
+                <div>
+                  <svg
+                    className="marker yellow"
+                    viewBox="0 0 24 24"
+                    width={12 * viewport.zoom}
+                    height={12 * viewport.zoom}
+                    stroke="currentColor"
+                    fill="none"
+                  >
+                    <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                    <circle cx="12" cy="10" r="3"></circle>
+                  </svg>
+                </div>
+              </Marker>
+              <Popup
+                latitude={addEntryLocation.latitude}
+                longitude={addEntryLocation.longitude}
+                dynamicPosition={true}
+                closeButton={true}
+                closeOnClick={false}
+                onClose={() => setAddEntryLocation(null)}
+                anchor="top" >
+                <div className="popup">
+                  <h3>Add your new log here</h3>
+                  <form onSubmit={handleSubmit} className="entry-form">
+                    <label for="title">Title:</label>
+                    <input type="text" id="title" name="title" />
+                    <label for="location">Location:</label>
+                    <input type="location" id="location" name="location" />
+                    <label for="comments">Comments:</label>
+                    <textarea row={3} type="comments" id="comments" name="comments" />
+                    <label for="image">Image:</label>
+                    <input type="text" id="image" name="image" />
+                    <label for="visitedDate">Visit Date: </label>
+                    <input type="date" id="visitedDate" name="visitedDate" />
+                    <button onClick={handleSubmit}>Create Log</button>
+                  </form>
+                </div>
+              </Popup>
+            </Fragment>
+          )}
         </Fragment>
-      ))}
-    </ReactMapGL>
+      ))
+      }
+    </ReactMapGL >
 
 
   );
